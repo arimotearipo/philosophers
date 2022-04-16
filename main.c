08ae4baa6b60fcd700713db5dab2b76402daac39
Wan nor adzahari Wan tajuddin <wwan-taj@u91z03s02.42kl.edu.my>
2022-04-16 15:29:02 +0800

    Created struct and main

--- main.c
+++ main.c
@@ -1,8 +1,40 @@
-#include <philo.h>
+#include "philo.h"
 
-#int	main(int ac, char **av)
+int	m = 0;
+pthread_mutex_t mutex;
+
+void	*eat(void *a)
 {
-	(void)ac;
-	(void)av;
+	(void)a;
+	printf("Eating");
+	return (0);
+}
+
+void	create_philo(t_life *life)
+{
+	int			i;
+
+	life->philos = malloc(sizeof(pthread_t) * life->philo_num);
+	i = 0;
+	while (i < life->philo_num)
+		pthread_create(&life->philos[i++], NULL, &eat, NULL);
+	i = 0;
+	while (i < life->philo_num)
+		pthread_join(life->philos[i++], NULL);
+}
+
+int	main(int ac, char **av)
+{
+	t_life	life;
+
+	life.philo_num = ft_atoi(av[1]);
+	life.time_to_die = ft_atoi(av[2]);
+	life.time_to_eat = ft_atoi(av[3]);
+	life.time_to_sleep = ft_atoi(av[4]);
+	if (ac == 6)
+		life.eat_num = ft_atoi(av[5]);
+	pthread_mutex_init(&mutex, NULL);
+	create_philo(&life);
+	pthread_mutex_destroy(&mutex);
 	return (0);
 }
--- philo.h
+++ philo.h
@@ -1,7 +1,20 @@
 #ifndef PHILO_H
 # define PHILO_H
 
+#include "libft/libft.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+typedef	struct	s_life
+{
+	pthread_t	*philos;
+	int			philo_num;
+	int			time_to_die;
+	int			time_to_eat;
+	int			time_to_sleep;
+	int			eat_num;
+}				t_life;
 
 #endif
